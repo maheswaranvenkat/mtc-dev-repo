8040bf471646a8cf705be8e226021e42f9653fbd
mahesh <maheswaran.venkatraman@gmail.com>
2022-06-23 20:30:07 +0530

    Managed by Terraform

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 //--------------------------------------------------------------------
 // Variables
 
-
+# ll
 
 //--------------------------------------------------------------------
 // Modules
 module "compute" {
   source  = "app.terraform.io/npl-terraform/compute/aws"
-  version = "1.0.11"
-  aws_region = "us-east-1"
+  version = "1.0.14"
+  aws_region = "us-west-1"
 
-  public_key_material = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCoR//MITnBACgAugld6WokYeP5wsxm4infZr6qz4bAG1IoOKGP9UHNmS1+vt1rcpmZep/YPuA0HmUBHaHvelvfvTSCGUGkcmuVKyo9fKotMbE7n1p3/FafQDJsvEVKltpm3pJurD+FA0YCINm4uXIu3NcIE6OdmFwhAa3HACT6Ov8Wfcw9kH0Lg9gQ0sjgymQgmC4fPi5zz7gFI8ciNZQZRcUdnIi51PjmgH8OKgOCWLvn0XNzuMd+Jj5n3CNW7AygjMZR7quJXVSEdGj8czxg5rC84nVtE9vsitRdBYw1IHAsP3fVZgMC65QT/wIiKNAiLL7JiK8S/FTkJXyEEmfx3tiv4Dud5ERApwBPUF0kE4JNK7Gt8LXMZKxCTdA0pW6SeVTEuELAVuFXeuejN/PndP0WymInmg27DuB00UHfPrWtmS+bv2YM3Aw8oxVhW4hLEbCKh/q11uc3VJ+zzMDm6y/wQliYuRnyUfaXaGU7FjHwdKcrSeCPF4/7kmclU4c= mahesh@Maheshs-MacBook-Pro.local"
+  public_key_material = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJY4sYcQxD7T2gORFCfzFoeadz8Ft6TCEfTAklKff+N+DvvLh3yzrpR3SvFK6ZsK7B8TACfxpAdSKOS53H8ko9bHr5b0001swGz27w17sB+XU/V8m7FjEFeZP/JNGBhoz62rIpkrXPb8JBe8nCsiQbtBeYltWCqwY/0bGVmjAKNQ2aNe+JvE+GfUj+7m5PPLmErbzScLWa6IuHfZON8iFZt4OZIOMmCqyLiUtElzPjQ1jqbUwvN6zp1N51sBh8695URpK165FczBNLMwHsq+jNGdGNXUI2+VKFSPxiVyd1GIknQbindijNVcHuJCmYa9MtMMtpPMU/2LNcKhk39v9r ubuntu@ip-172-31-81-5"
   public_sg = module.networking.public_sg
   public_subnets = module.networking.public_subnets
 }
@@ -20,5 +20,5 @@ module "networking" {
   version = "1.0.0"
 
   access_ip = "0.0.0.0/0"
-  aws_region = "us-east-1"
+  aws_region = "us-west-1"
 }
